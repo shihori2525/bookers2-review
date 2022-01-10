ec0a81e5cde0add1c61ed7f9543f44b53da2fe31
EC2 Default User <ec2-user@ip-172-31-95-103.ec2.internal>
2022-01-10 12:15:55 +0000

    [Fix]create book

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+    @user = current_user
+  end
 
   def edit
     @user = User.find(params[:id])
@@ -25,7 +27,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image)
+    params.require(:user).permit(:name,:email, :introduction, :profile_image)
   end
 
   def ensure_correct_user
