9d19e7df3c05ff6b9b9b22cae844aaabf78c8bc9
sakata shihori <rirakkuma.250@gmail.com>
2022-01-15 14:48:47 +0000

    [Add]favorite link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
-    @book = Book.new
+    @book_new = Book.new
+    @book = Book.find(params[:id])
   end
 
   def index
